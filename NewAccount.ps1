239a682643a7d8b05bb2c71e4c64756f3bd97da6
Nathan Taylor <nathanvinding@gmail.com>
2021-09-07 12:58:35 -0400

    the whole thing, thats how you use git right?

--- NewAccount.ps1
+++ NewAccount.ps1
@@ -163,55 +163,11 @@ Set-TimeZone -Id "Eastern Standard Time" -PassThru
 # Install Adobe Reader
 #===================================================
 
-
+#the below part works, but it's ugly
 <#
-
-# Silent install Adobe Reader DC
-# https://get.adobe.com/nl/reader/enterprise/
-
-# Path for the workdir
-$workdir = "c:\installer\"
-
-# Check if work directory exists if not create it
-
-If (Test-Path -Path $workdir -PathType Container)
-{ Write-Host "$workdir already exists" -ForegroundColor Red}
-ELSE
-{ New-Item -Path $workdir  -ItemType directory }
-
-# Download the installer
-
-$source = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1502320053/AcroRdrDC1502320053_en_US.exe"
-$destination = "$workdir\adobeDC.exe"
-
-# Check if Invoke-Webrequest exists otherwise execute WebClient
-
-if (Get-Command 'Invoke-Webrequest')
-{
-     Invoke-WebRequest $source -OutFile $destination
-}
-else
-{
-    $WebClient = New-Object System.Net.WebClient
-    $webclient.DownloadFile($source, $destination)
-}
-
-# Start the installation
-
-Start-Process -FilePath "$workdir\adobeDC.exe" -ArgumentList "/sPB /rs"
-
-# Wait XX Seconds for the installation to finish
-
-Start-Sleep -s 35
-
-# Remove the installer
-
-rm -Force $workdir\adobe*
-#>
-
 Copy-Item D:\adobe\readerdc_en_xa_crd_install.exe D:\not_adobe.exe
 Rename-Item -path D:\not_adobe.exe -NewName readerdc_en_xa_crd_install.exe
 
 &D:\readerdc_en_xa_crd_install.exe
-
+#>
 
